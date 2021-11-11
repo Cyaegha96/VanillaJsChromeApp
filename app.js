b86ee54256b41a43e8b8212a27a01ceb17e61aed
Cyaegha96 <zazae51@gmail.com>
2021-11-11 14:48:50 +0900

    saveUsername

--- app.js
+++ app.js
@@ -3,14 +3,30 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
   loginForm.classList.add(HIDDEN_CLASSNAME);
 
-  greeting.innerText = "hello " + username;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, username);
+
+  paintGreeting(username);
+
   console.log(greeting);
 }
-loginForm.addEventListener("submit", onLoginSubmit);
+
+function paintGreeting(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername === null) {
+  //show userform
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreeting(savedUsername);
+}
